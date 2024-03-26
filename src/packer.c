096de23e0606d12ba019c8a435ddaf8f486a6358
VictorJVA <130608466+VictorJVA@users.noreply.github.com>
2024-03-26 15:01:49 +0000

    remaining_length

--- src/packer.c
+++ src/packer.c
@@ -1,8 +1,29 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
-uint8_t next_byte(uint8_t **buffer){
+uint8_t next_byte(uint8_t **buffer) {
   uint8_t byte = **buffer;
   (*buffer)++;
   return byte;
+}
+
+void pack_byte(uint8_t **buf, uint8_t val) {
+  **buf = val;
+  (*buf) += sizeof(uint8_t);
+}
+
+uint32_t remaining_length(const uint8_t **buf) {
+
+  char c;
+  int multiplier = 1;
+  uint32_t value = 0LL;
+
+  do {
+    c = **buf;
+    value += (c & 127) * multiplier;
+    multiplier *= 128;
+    (*buf)++;
+  } while ((c & 128) != 0);
+
+  return value;
 }
--- src/packer.h
+++ src/packer.h
@@ -4,5 +4,8 @@
 #include <stdint.h>
 
 uint8_t next_byte(uint8_t **buffer);
+void pack_byte(uint8_t **buf, uint8_t val);
+uint32_t remaining_length(const uint8_t **buf);
+
 
 #endif
